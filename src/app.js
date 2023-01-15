5339e448e488a27d075922f448001db0af77be59
gsdutra <gabrielsprecherdutra@gmail.com>
2023-01-15 14:02:23 -0300

    Created route POST '/participants'

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import express from "express";
 import { MongoClient } from "mongodb";
+import cors from 'cors';
+import joi from 'joi';
 
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const PORT = 5000;
 
@@ -13,22 +16,44 @@ const mongoClient = new MongoClient(process.env.DATABASE_URL);
 let db;
 
 mongoClient.connect().then(() => {
-	db = mongoClient.db("meu_banco_de_dados");
+	db = mongoClient.db();
 });
 
 
 //GET routes
 app.get("/participants", (req, res) => {
-	res.sendStatus(201)
+	res.sendStatus(200)
 });
 
 app.get("/messages", (req, res) => {
-	res.sendStatus(201)
+	res.sendStatus(200)
 });
 
 
 //POST routes
-app.post("/participants", (req, res) => {
+app.post("/participants", async (req, res) => {
+	const name = req.body;
+	const nameSchema = joi.object({
+		name: joi.string().required()
+	})
+	const validation = nameSchema.validate(name);
+
+	if (validation.error){
+		return res.status(422).send(validation.error.details)
+	}
+
+	console.log(db.collection("participants").findOne(name))
+
+	const alreadyExists = await db.collection("participants").findOne(name)
+
+	if (alreadyExists){
+
+		return res.sendStatus(409)
+	}
+	db.collection("participants").insertOne({
+		name: name.name,
+		lastStatus: Date.now()
+	})
 	res.sendStatus(201)
 });
 
